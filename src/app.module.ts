47c6da0c3925a1215f1b0172c3071ee0f35fed4d
ahlem-am <ahlemammar16@gmail.com>
2023-11-26 12:20:04 +0100

    location service

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,10 @@ import { Location,LocationSchema } from './location.schema';
   
     //CN6OQY2u7pGTBBxF
     //'mongodb+srv://ahlemam:YC2JDEEj5KtIeU4t@cluster0.zd4x5hp.mongodb.net/ahlem?retryWrites=true&w=majority&ssl=true'
-   
-    MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: 'mongodb+srv://ahlem_:vaDgaXlRZUiKLyps@cluster0.zd4x5hp.mongodb.net/ahlem?retryWrites=true&w=majority&ssl=true',
-     
-        
-  
-      }),
-    }),
+   //'mongodb+srv://<username>:<password>@cluster0.zd4x5hp.mongodb.net/?retryWrites=true&w=majority'
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017'
+      ),
     MongooseModule.forFeature([{ name:Location.name, schema:LocationSchema }])
   ],
   controllers: [AppController],
